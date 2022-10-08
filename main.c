68abb4d6cc6fe07136cd618e0653277962c4eea3
DavidMedina <david.medina@cidecuc.mx>
2022-10-08 09:06:28 -0500

    arreglar issue_1

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 int main (void){
     printf("Hola mundo");
+//CAmbio 1 en el main
+//CAmbio 1 en el main
+//CAmbio 1 en el main
+//CAmbio 1 en el main
 
-    //CAmbio 1 en el main
+printf("Cerrar Ventana");
 }
